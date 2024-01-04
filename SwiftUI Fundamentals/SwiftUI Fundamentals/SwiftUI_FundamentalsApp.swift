d4df9d241671e9f2cb8993917cff8b63deb6ccc7
Akshay Kumar <akshaychaudhary55@gmail.com>
2024-01-04 10:43:25 +0530

    initial commit

---
+++ SwiftUI Fundamentals/SwiftUI Fundamentals/SwiftUI_FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FundamentalsApp.swift
+//  SwiftUI Fundamentals
+//
+//  Created by Akshay Kumar on 04/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
