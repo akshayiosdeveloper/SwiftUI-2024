b5dfe8f98cb19db8949d57f9935c0284c6dcc75e
Akshay Kumar <akshaychaudhary55@gmail.com>
2024-01-05 11:52:25 +0530

    learned about spacing

--- SwiftUI Fundamentals/SwiftUI Fundamentals/ContentView.swift
+++ SwiftUI Fundamentals/SwiftUI Fundamentals/ContentView.swift
@@ -9,16 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("VStack")
+                .font(.largeTitle)
+            Text("Alignment")
+                .font(.title)
+                .foregroundColor(.gray)
+            Text("By default, views in a VStack are center aligned.")
+            VStack(alignment: .leading, spacing: 40) {
+                Text("Leading Alignment")
+                    .font(.title)
+                Divider() // Creates a thin line (Push-out view)
+                Image(systemName: "arrow.left")
+            }
+            .padding()
+            .foregroundColor(Color.white)
+            .background(RoundedRectangle(cornerRadius: 10)
+            .foregroundColor(.blue))
+            .padding()
+            VStack(alignment: .trailing, spacing: 40) {
+                Text("Trailing Alignment")
+                    .font(.title)
+                Divider()
+                Image(systemName: "arrow.right")
+            }
+            .padding()
+            .foregroundColor(Color.white)
+            .background(RoundedRectangle(cornerRadius: 10)
+            .foregroundColor(.blue))
+            .padding()
+            VStack(alignment:.trailing,spacing:10) {
+                Text("Hello")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(RoundedRectangle(cornerRadius: 20))
+                    .foregroundColor(.gray)
+            }
         }
-        .padding()
-    }
+        .font(.title)
+       }
 }
 
 #Preview {
     ContentView()
+        .environment(\.sizeCategory, .extraSmall)
 }
