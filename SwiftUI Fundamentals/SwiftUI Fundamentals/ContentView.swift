6b14ebeafdfe04a3cb79003be975f366116bb268
Akshay Kumar <akshaychaudhary55@gmail.com>
2024-01-08 12:31:37 +0530

    read about images

--- SwiftUI Fundamentals/SwiftUI Fundamentals/ContentView.swift
+++ SwiftUI Fundamentals/SwiftUI Fundamentals/ContentView.swift
@@ -9,27 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-    VStack(spacing: 20.0) {
-//    HeaderView("LazyVStack",
-//    subtitle: "Alignment",
-//    desc: "Since LazyVStacks are push-out views (horizontally) the alignment
-//    parameter could be useful.")
-    Text("Leading")
-    LazyVStack(alignment: .leading, spacing: 40) {
-    Image(systemName: "1.circle")
-    Image(systemName: "2.circle")
-    Image(systemName: "3.circle")
-    }
-    .border(Color.red, width: 2) // Draws a border around the frame of the view
-    Text("Trailing")
-    LazyVStack(alignment: .trailing, spacing: 40) {
-    Image(systemName: "1.circle")
-    Image(systemName: "2.circle")
-    Image(systemName: "3.circle")
-    }
-    }
-    .border(Color.red, width: 2)
-    .font(.title)
+        //Image(systemName: "cloud.heavyrain")
+        //            .font(.system(size: 200))
+        //            .foregroundColor(.blue)
+        //            .shadow(color: .gray, radius: 10,x: 10,y: 10)
+//        Image("flipbox")
+//            .resizable()
+//            .aspectRatio(contentMode: .fit)
+//            .frame(width: 300)
+//            .overlay(
+//                Text("If you are lucky enough to have lived in Paris as a young man, then wherever you go for the rest of your life it stays with you, for Paris is a moveab le feast.\n\n- Ernest Hemingway")
+//                    .fontWeight(.heavy)
+//                    .font(.system(.headline, design: .rounded))
+//                    .foregroundColor(.white)
+//                    .padding()
+//                    .background(Color.black)
+//                    .cornerRadius(10)
+//                    .opacity(0.8)
+//                    .padding(),
+//                alignment: .top
+//            )
+//        Image("flipbox")
+//            .resizable()
+//            .aspectRatio(contentMode: .fit)
+//            .overlay(
+//                Color.black
+//                    .opacity(0.6)
+//        )
+        Image("flipbox")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 300)
+            .overlay(
+                Color.black
+                    .opacity(0.4)
+                    .overlay(
+                        Text("Paris")
+                            .font(.largeTitle)
+                            .fontWeight(.black)
+                            .foregroundColor(.white)
+                            .frame(width: 200)
+        ) )
     }
 }
 
