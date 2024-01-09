fc6d2daaa32abe3a72052d9ac32548fdff58d2a2
Akshay Kumar <akshaychaudhary55@gmail.com>
2024-01-09 11:21:39 +0530

    studied about images

--- SwiftUI Fundamentals/SwiftUI Fundamentals/ContentView.swift
+++ SwiftUI Fundamentals/SwiftUI Fundamentals/ContentView.swift
@@ -9,47 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        //Image(systemName: "cloud.heavyrain")
-        //            .font(.system(size: 200))
-        //            .foregroundColor(.blue)
-        //            .shadow(color: .gray, radius: 10,x: 10,y: 10)
-//        Image("flipbox")
-//            .resizable()
-//            .aspectRatio(contentMode: .fit)
-//            .frame(width: 300)
-//            .overlay(
-//                Text("If you are lucky enough to have lived in Paris as a young man, then wherever you go for the rest of your life it stays with you, for Paris is a moveab le feast.\n\n- Ernest Hemingway")
-//                    .fontWeight(.heavy)
-//                    .font(.system(.headline, design: .rounded))
-//                    .foregroundColor(.white)
-//                    .padding()
-//                    .background(Color.black)
-//                    .cornerRadius(10)
-//                    .opacity(0.8)
-//                    .padding(),
-//                alignment: .top
-//            )
-//        Image("flipbox")
-//            .resizable()
-//            .aspectRatio(contentMode: .fit)
-//            .overlay(
-//                Color.black
-//                    .opacity(0.6)
-//        )
-        Image("flipbox")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 300)
-            .overlay(
-                Color.black
-                    .opacity(0.4)
-                    .overlay(
-                        Text("Paris")
-                            .font(.largeTitle)
-                            .fontWeight(.black)
-                            .foregroundColor(.white)
-                            .frame(width: 200)
-        ) )
+    ImageView()
     }
 }
 
---
+++ SwiftUI Fundamentals/SwiftUI Fundamentals/ImageView.swift
@@ -0,0 +1,62 @@
+//
+//  ImageView.swift
+//  SwiftUI Fundamentals
+//
+//  Created by Akshay Kumar on 09/01/24.
+//
+
+import SwiftUI
+
+struct ImageView: View {
+    var body: some View {
+        //Image(systemName: "cloud.heavyrain")
+        //            .font(.system(size: 200))
+        //            .foregroundColor(.blue)
+        //            .shadow(color: .gray, radius: 10,x: 10,y: 10)
+//        Image("flipbox")
+//            .resizable()
+//            .aspectRatio(contentMode: .fit)
+//            .frame(width: 300)
+//            .overlay(
+//                Text("If you are lucky enough to have lived in Paris as a young man, then wherever you go for the rest of your life it stays with you, for Paris is a moveab le feast.\n\n- Ernest Hemingway")
+//                    .fontWeight(.heavy)
+//                    .font(.system(.headline, design: .rounded))
+//                    .foregroundColor(.white)
+//                    .padding()
+//                    .background(Color.black)
+//                    .cornerRadius(10)
+//                    .opacity(0.8)
+//                    .padding(),
+//                alignment: .top
+//            )
+//        Image("flipbox")
+//            .resizable()
+//            .aspectRatio(contentMode: .fit)
+//            .overlay(
+//                Color.black
+//                    .opacity(0.6)
+//        )
+//        Image("flipbox")
+//            .resizable()
+//            .aspectRatio(contentMode: .fit)
+//            .frame(width: 300)
+//            .overlay(
+//                Color.black
+//                    .opacity(0.4)
+//                    .overlay(
+//                        Text("Paris")
+//                            .font(.largeTitle)
+//                            .fontWeight(.black)
+//                            .foregroundColor(.white)
+//                            .frame(width: 200)
+//        ) )
+        Image(systemName:"slowmo", variableValue: 0.5)
+            .symbolRenderingMode(.palette)
+            .foregroundStyle(.indigo)
+            .font(.largeTitle)
+    }
+}
+
+#Preview {
+    ImageView()
+}
