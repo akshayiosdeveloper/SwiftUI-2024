42e48b18b2ba2d1229d589c23dc7b458de1b4852
Akshay Kumar <akshaychaudhary55@gmail.com>
2024-02-12 13:54:54 +0530

    start with statement

---
+++ SwiftUI Fundamentals/SwiftUI Fundamentals/StateManagement.swift
@@ -0,0 +1,18 @@
+//
+//  StateManagement.swift
+//  SwiftUI Fundamentals
+//
+//  Created by Akshay Kumar on 12/02/24.
+//
+
+import SwiftUI
+
+struct StateManagement: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StateManagement()
+}
