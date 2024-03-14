3d8be4b30e428cb7f2a700d59a6731845d0eb859
Akshay Kumar <akshay.kumar@system73.com>
2024-03-14 11:52:01 +0530

    woring on list

---
+++ SwiftUI Fundamentals/SwiftUI Fundamentals/ListView/ListView.swift
@@ -0,0 +1,21 @@
+//
+//  ListView.swift
+//  SwiftUI Fundamentals
+//
+//  Created by Akshay Kumar on 14/03/24.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petit e Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli" , "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donos tia", "Royal Oak", "CASK Pub and Kitchen"]
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petite oyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate" , "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "five leaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpub kitchen"]
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
