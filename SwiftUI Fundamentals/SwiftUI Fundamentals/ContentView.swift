7bdfc40119c361988afe499d02cf32da30cde74f
Akshay Kumar <akshay.kumar@system73.com>
2024-04-18 16:39:34 +0530

    working on sheets

--- SwiftUI Fundamentals/SwiftUI Fundamentals/ContentView.swift
+++ SwiftUI Fundamentals/SwiftUI Fundamentals/ContentView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigatinStack_Exercise()
+        SheetExample()
+       // NavigatinStack_Exercise()
        // NavigationStack()
         //ListView()
        // AnimationChallenge()
