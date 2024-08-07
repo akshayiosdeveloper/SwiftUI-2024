92edbf96a0da64223364593ddd7910f24704f0f6
Akshay Kumar <akshay.kumar@system73.com>
2024-08-07 10:49:14 +0530

    complete presentation detent chapter

--- SwiftUI Fundamentals/SwiftUI Fundamentals/PresentationDetents/BottomSheetDemo.swift
+++ SwiftUI Fundamentals/SwiftUI Fundamentals/PresentationDetents/BottomSheetDemo.swift
@@ -8,18 +8,31 @@
 import SwiftUI
 
 struct BottomSheetDemo: View {
+    @State private var selectedDetent: PresentationDetent = .medium
+    @State private var selectedRestaurant: RestaurantDemo?
     var body: some View {
         let restaurantsCollection = FormDemo().restaurants
         NavigationStack {
             List {
                 ForEach(restaurantsCollection) { restaurant in
                     BasicImageRow(restaurant: restaurant)
+                        .onTapGesture {
+                                self.selectedRestaurant = restaurant
+                            }
                 }
             }
             .listStyle(.plain)
             
             .navigationTitle("Restaurants")
         }
+        .sheet(item: $selectedRestaurant) { restaurant in
+            RestaurantDetailViewDemo(restaurant: restaurant)
+                .presentationDetents([.height(200), .medium, .large], selection: $selectedDetent)
+//                .presentationDetents([.fraction(0.1), .height(200), .medium, .large])
+               // .presentationDetents([.medium, .large])
+                .presentationDragIndicator(.hidden)
+        }
+       
     }
 }
 
--- SwiftUI Fundamentals/SwiftUI Fundamentals/PresentationDetents/RestaurantDetailViewDemo.swift
+++ SwiftUI Fundamentals/SwiftUI Fundamentals/PresentationDetents/RestaurantDetailViewDemo.swift
@@ -8,13 +8,26 @@
 import SwiftUI
 
 struct RestaurantDetailViewDemo: View {
+    let restaurant: RestaurantDemo
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            HandleBar()
+            TitleBar()
+            HeaderView123(restaurant: self.restaurant)
+            DetailInfoView(icon: "map", info: "India")
+                .padding(.top)
+            DetailInfoView(icon: "phone", info: self.restaurant.phone) 
+            DetailInfoView(icon: nil, info: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32")
+                .padding(.top)
+        }
+        .background(Color.white)
+        .cornerRadius(10, antialiased: true)
     }
 }
-
+   
 #Preview {
-    RestaurantDetailViewDemo()
+    RestaurantDetailViewDemo(restaurant:FormDemo().restaurants[0])
 }
 
 struct HandleBar: View {
@@ -27,3 +40,59 @@ struct HandleBar: View {
     }
 }
 
+struct TitleBar: View {
+    var body: some View {
+        HStack {
+            Text("Restaurant Details")
+                .font(.headline)
+                .foregroundColor(.primary)
+            Spacer() }
+        .padding() }
+}
+
+struct HeaderView123: View {
+    let restaurant: RestaurantDemo
+    var body: some View {
+        Image(restaurant.image)
+            .resizable()
+            .scaledToFill()
+            .frame(height: 300)
+            .clipped()
+            .overlay(
+                HStack {
+                    VStack(alignment: .leading) {
+                        Spacer()
+                        Text(restaurant.name)
+                            .foregroundColor(.white)
+                            .font(.system(.largeTitle, design: .rounded))
+                            .bold()
+                        Text(restaurant.type)
+                            .font(.system(.headline, design: .rounded))
+                            .padding(5)
+                            .foregroundColor(.white)
+                            .background(Color.red)
+                            .cornerRadius(5)
+                    }
+                    Spacer() }
+                    .padding() )
+    }
+}
+
+struct DetailInfoView: View {
+    let icon: String?
+    let info: String
+    var body: some View  {
+        HStack {
+            if icon != nil {
+                Image(systemName: icon!)
+                    .padding(.trailing, 10)
+                
+            }
+            Text(info)
+                .font(.system(.body, design: .rounded))
+            Spacer()
+            
+        }.padding(.horizontal)
+        
+    }
+}
