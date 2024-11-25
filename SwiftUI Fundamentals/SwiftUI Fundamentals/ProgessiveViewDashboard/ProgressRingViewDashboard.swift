fe41d718b600bf998e42c5a700aa05b633e5c2e8
Akshay Kumar <akshay.kumar@system73.com>
2024-11-25 10:55:24 +0530

    add color extension

---
+++ SwiftUI Fundamentals/SwiftUI Fundamentals/ProgessiveViewDashboard/ProgressRingViewDashboard.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressRingViewDashboard.swift
+//  SwiftUIFundamentals
+//
+//  Created by Akshay Kumar on 25/11/24.
+//
+
+import SwiftUI
+
+struct ProgressRingViewDashboard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressRingViewDashboard()
+}
