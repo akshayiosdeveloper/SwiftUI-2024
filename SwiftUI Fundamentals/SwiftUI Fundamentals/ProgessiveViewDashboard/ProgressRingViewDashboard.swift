dd29293bb5e26768ac8c10b2364aba37f7cff218
Akshay Kumar <akshay.kumar@system73.com>
2024-12-03 11:06:53 +0530

    add progressive text

--- SwiftUI Fundamentals/SwiftUI Fundamentals/ProgessiveViewDashboard/ProgressRingViewDashboard.swift
+++ SwiftUI Fundamentals/SwiftUI Fundamentals/ProgessiveViewDashboard/ProgressRingViewDashboard.swift
@@ -19,7 +19,10 @@ struct ProgressRingViewDashboard: View {
     var body: some View {
         ZStack {
             Circle().stroke(Color(.systemGray6),lineWidth: thickness)
+            
             RingShape(progress: progress, thickness: thickness) .fill(AngularGradient(gradient: gradient, center: .center, startAngle: .degrees(startAngle), endAngle: .degrees(360 * progress + startAngle)))
+                .animatableProgressText(progress: progress)
+            
             RingTip(progress: progress, startAngle: startAngle, ringRadius: radius) .frame(width: thickness, height: thickness)
             .foregroundColor(progress > 0.96 ? gradient.stops[1].color : Color.clear)
             .shadow(color: progress > 0.96 ? Color.black.opacity(0.15) : Color.clear, radius: 2 , x: ringTipShadowOffset.x, y: ringTipShadowOffset.y)
@@ -86,3 +89,39 @@ struct RingTip: Shape {
 //            .previewDisplayName("ProgressRingView (90%)")
 //    }
 //}
+
+struct ProgressTextModifier : AnimatableModifier {
+    
+    var progress: Double = 0.0
+    var textColor: Color = .primary
+    
+    private var progressText: String {
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .percent
+    formatter.percentSymbol = "%"
+    return formatter.string(from: NSNumber(value: progress)) ?? ""
+    }
+    
+    var animatableData: Double {
+    get { progress }
+    set { progress = newValue }
+    }
+  
+    func body(content: Content) -> some View {
+        content
+            .overlay {
+                Text(progressText)
+                .font(.system(.largeTitle, design: .rounded))
+                .fontWeight(.bold)
+                .foregroundColor(textColor)
+                .animation(nil)
+            }
+    }
+    
+}
+
+extension View {
+    func animatableProgressText(progress: Double, textColor: Color = Color.primary) -> some View {
+        self.modifier(ProgressTextModifier(progress: progress, textColor: textColor))
+    }
+}
